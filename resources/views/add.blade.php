89dcce4439359c377e1fff336fc8abb4b6715d40
georgeksaji <georgeksaji14@gmail.com>
2025-02-21 10:36:00 +0530

    Updated Add City with current location button

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -5,19 +5,21 @@
     <link rel="shortcut icon" href="icon.png" type="image/x-icon">
     <link rel="icon" href="icon.png" type="image/x-icon">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Atkinson+Hyperlegible+Next:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Atkinson+Hyperlegible+Next:ital,wght@0,200..800;1,200..800&display=swap"
+        rel="stylesheet">
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Login:WB</title>
     <style>
-        
-.atkinson-hyperlegible-next-text-font {
-  font-family: "Atkinson Hyperlegible Next", serif;
-  font-optical-sizing: auto;
-  font-weight: bold;
-  font-style: normal;
-}
+        .atkinson-hyperlegible-next-text-font {
+            font-family: "Atkinson Hyperlegible Next", serif;
+            font-optical-sizing: auto;
+            font-weight: bold;
+            font-style: normal;
+        }
+
         body {
             margin: 0;
             padding-block: 1%;
@@ -33,42 +35,48 @@
             height: 100vh;
             width: 100vw;
         }
-        .topbar
-        {
+
+        .topbar {
             background-color: #1e1e1e;
             width: 100%;
             font-family: Arial, sans-serif;
             font-size: 13px;
-            padding-inline:10px;
+            padding-inline: 10px;
             color: white;
         }
+
         a:link {
             color: white;
             text-decoration: none;
         }
+
         a:visited {
             color: white;
             text-decoration: none;
         }
+
         .sidebar {
             width: 55%;
             background-color: #1e1e1e;
             padding: 20px;
             border-radius: 10px;
             height: 60%;
-            transition: all 0.3s ease;
+            min-height: inherit;
+            transition: all 0.3sease;
+
         }
+
         .sidebar:hover {
             transform: scale(1.02);
         }
 
-        .login{
-            display: flex
-;
-    justify-content: center;
-    align-items: center;
-    height: 26%;
-    padding-block: 1%;
+        .login {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 50%;
+            width: 100%;
+            padding-block: 1%;
         }
 
         .ad-btn {
@@ -78,7 +86,11 @@
             padding: 10px 20px;
             border-radius: 5px;
             font-weight: bold;
+            max-height: fit-content;
             transition: all 0.3s ease;
+            margin-inline: 1%;
+            display: block;
+            align-items: center;
         }
 
         .ad-btn:hover {
@@ -90,27 +102,39 @@
             display: flex;
             justify-content: center;
             align-items: center;
+            width: 38vh;
         }
-        input[type="text"],input[type="password"] {
-            width: 280px;
+
+        .buttons-inner {
+            display: flex;
+            justify-content: space-between;
+            width: 99%;
+        }
+
+        input[type="text"],
+        input[type="password"] {
+            width: 100%;
             padding: 10px;
-            border-radius:10px;
+            border-radius: 10px;
             border: none;
             outline: none;
             font-family: Arial, sans-serif;
             font-size: 14px;
         }
+
         .title {
             color: #ffffff;
             font-size: 30px;
             font-weight: bold;
             text-align: center;
         }
+
         .logo {
             display: flex;
             justify-content: center;
             align-items: center;
         }
+
         form {
             display: grid;
             justify-content: center;
@@ -118,6 +142,7 @@
             height: 100%;
             width: 100%;
         }
+
         .linkstyle {
             color: #ffffff;
             font-size: 12px;
@@ -127,9 +152,8 @@
             display: block;
             margin-top: 5px;
         }
-        input
-        {
 
+        input {
             border: none;
             outline: none;
             font-family: Arial, sans-serif;
@@ -141,26 +165,42 @@
 </head>
 
 <body>
-        
-        <div class="sidebar">
+
+    <div class="sidebar">
         <div class="topbar"><a href="/home">Home->Add City</a></div>
-            <div class="logo">
-                <img src="logo.png" alt="logo" height="100px" />
-            </div>
-            <div class="title atkinson-hyperlegible-next-text-font">Add City
-            <div class="topbar" style="text-align:center"><p><img src="current-location.png" height="15px" alt="">&nbsp{{ $city}},&nbsp{{ $region }},&nbsp{{ $country }}</p></div>
-             </div>
-            <div class="login">
-                <form action="" method="post">
-                    <input type="text" name="city" value={{ $city }} placeholder="City" required />
-                    <input type="text" name="country" value={{ $country }} placeholder="Country" required />
-               </form>
-            </div>
-            <div class="buttons">
-                <button class="ad-btn logout"><img src="send.png" height="15px" alt="Fav" />&nbspSend</button>
+        <div class="logo">
+            <img src="logo.png" alt="logo" height="100px" />
+        </div>
+        <div class="title atkinson-hyperlegible-next-text-font">Add City
+            <div class="topbar" style="text-align:center">
+                <p><img src="current-location.png" height="15px"
+                        alt="">&nbsp{{ $city}},&nbsp{{ $region }},&nbsp{{ $country }}</p>
             </div>
         </div>
-  
+        <div class="login">
+            <form action="" method="post">
+                <input type="text" id="city" name="city" placeholder="City" required />
+                <input type="text" id="country" name="country" placeholder="Country" required />
+                <div class="buttons">
+                    <div class="buttons-inner">
+                        <button class="ad-btn logout" id="getcurloc"><img src="current-location.png" height="15px"
+                                alt="Curr" />&nbspCurrent Location</button>
+                        <button class="ad-btn logout"><img src="save.png" height="15px" alt="Save" />&nbspSave</button>
+                    </div>
+            </form>
+        </div>
+    </div>
+    </div>
+    <script>
+        document.getElementById("getcurloc").addEventListener("click", function (event) {
+            event.preventDefault(); // Prevent form submission
+
+            // Just set the existing PHP variables to the input fields
+            document.getElementById("city").value = "{{ $city }}";
+            document.getElementById("country").value = "{{ $country }}";
+        });
+    </script>
+
 </body>
 
 </html>
